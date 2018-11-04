39d3627fe4d34413b7ccf2f78297618d2a8938f9
Feng Lin <feng.lin@ubnt>
2018-11-04 21:46:28 +0200

    Add babel and webpack.

---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './public/javascripts/main.js',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  resolve: {
+    modules: [
+      path.resolve(__dirname, "public"), 
+      "node_modules"
+    ]
+  },
+  plugins: [
+    //new CleanWebpackPlugin(['public/js']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public/js'),
+    pathinfo: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test : /\.pug$/,
+        use: [
+          {
+            loader: 'pug-loader',
+            options: {
+              pretty:  true,
+            }
+          }
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
+  }
+};
