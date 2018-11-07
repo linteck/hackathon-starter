75ff445948560b432943f6ccf64f789046ca86ab
Feng Lin <feng.lin@ubnt>
2018-11-07 20:39:05 +0200

    Show calendar.

--- views/calendar.js
+++ views/calendar.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'font-awesome/css/font-awesome.min.css'
+import 'react-big-calendar/lib/less/styles.less'
+//import './styles.less'
+//import './prism.less'
 import BigCalendar from 'react-big-calendar'
 import moment from 'moment'
-import myEventsList from 'events'
+import myEventsList from 'eventslist'
 //
 // Setup the localizer by providing the moment (or globalize) Object
 // to the correct localizer.
@@ -16,6 +21,6 @@ const ExCalendar = props => (
       endAccessor="end"
     />
   </div>
-);
+)
 
-export default ExCalendar;
+export default ExCalendar
--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,16 @@ module.exports = {
         ]
       },
       {
+        test: /\.less$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'less-loader' // compiles Less to CSS
+        }]
+      },
+      {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           'file-loader'
